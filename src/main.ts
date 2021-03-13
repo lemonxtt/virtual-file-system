82e44ed716db002cdce7c9a8f70cdcc01119456b
Quan Nguyen <hong-quan.nguyen@ant-tech.eu>
2021-03-13 15:50:16 +0700

    add middlewares

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as morgan from 'morgan'
+import * as helmet from 'helmet'
+import * as compression from 'compression'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,6 +16,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+
+  app.enableCors()
+  app.use(morgan('dev'))
+  app.use(helmet())
+  app.use(compression())
 
   const port = 3001
   await app.listen(port).then(() => console.log("Server is running on port " + port))
